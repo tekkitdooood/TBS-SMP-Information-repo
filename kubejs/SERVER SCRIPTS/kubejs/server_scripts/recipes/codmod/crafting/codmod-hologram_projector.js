e8534598b0875167a781b1aa94acbaf99618d7d7
tekkitdooood <jimboswitch3@gmail.com>
2026-01-21 10:04:14 -0500

    new sfx, handcannon lang fixed (??)

--- kubejs/SERVER SCRIPTS/kubejs/server_scripts/recipes/codmod/crafting/codmod-hologram_projector.js
+++ kubejs/SERVER SCRIPTS/kubejs/server_scripts/recipes/codmod/crafting/codmod-hologram_projector.js
@@ -1,27 +1,16 @@
 ServerEvents.recipes(event => {
-    event.custom({
-        "type": "minecraft:crafting_shaped",
-  "pattern": [
-    "IGI",
-    "IDI",
-    "ICI"
-  ],
-  "key": {
-    "I": {
-      "item": "minecraft:iron_ingot"
-    },
-    "D": {
-      "item": "minecraft:diamond"
-    },
-    "G": {
-      "item": "minecraft:tinted_glass"
-    },
-    "C": {
-      "item": "minecraft:redstone_dust"
-    }
-  },
-  "result": {
-    "id": "codmod:hologram_projector",
-    "count": 1
-    }})
+    event.shaped(
+        Item.of("codmod:hologram_projector", 1),
+        [
+            "IGI",
+            "IDI",
+            "ICI"
+        ],
+        {
+            I: "minecraft:iron_ingot",
+            D: "minecraft:diamond",
+            G: "minecraft:tinted_glass",
+            C: "minecrafT:redstone_dust"
+        }
+    )
 })
